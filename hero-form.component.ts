2eb6e258d4e198a8bc092a0a09c7294de2a67916
Carlos <carlos.fattor@gmail.com>
2016-07-12 01:55:18 -0300

    update form

--- hero-form.component.ts
+++ hero-form.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
-import { NgForm }    from '@angular/forms';
+import { NgForm, FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, FormBuilder, FormGroup, Validators, AbstractControl }    from '@angular/forms';
 import { Hero }   from './models/Hero';
 @Component({
   selector: 'hero-form',
+  directives: [FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES],
   templateUrl: 'app/hero-form.component.html'
 })
 export class HeroFormComponent {
+  myForm: FormGroup;
+  sku: AbstractControl;
+  constructor(_fb: FormBuilder) {
+    this.myForm = _fb.group({
+      'sku': ['', Validators.required]
+    });
+    this.sku = this.myForm.controls['sku'];
+  }
+  onSubmit2(form: any): void {
+    console.log('you submitted value:', form);
+  }
+
+
   powers = ['Really Smart', 'Super Flexible',
-            'Super Hot', 'Weather Changer'];
+    'Super Hot', 'Weather Changer'];
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
   submitted = false;
-  onSubmit() { this.submitted = true; }
+  
+  onSubmit(hero: Hero) {
+    console.log("hero=> " + JSON.stringify(hero));
+    this.submitted = true;
+  }
+
   // Reset the form with a new hero AND restore 'pristine' class state
   // by toggling 'active' flag which causes the form
   // to be removed/re-added in a tick via NgIf
--- main.ts
+++ main.ts
@@ -2,6 +2,5 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { AppComponent } from './app.component';
 bootstrap(AppComponent,[
-  disableDeprecatedForms(),
   provideForms()
  ]);
